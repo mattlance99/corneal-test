8dc8238d9080d1547d565a41ac0820d187cc11ee
Matthew Cohen <matt.cohen182@gmail.com>
2019-01-01 20:06:58 +0000

    first commit

---
+++ lib/corneal/test.rb
@@ -0,0 +1,8 @@
+require "corneal/test/version"
+
+module Corneal
+  module Test
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/corneal/test/version.rb
@@ -0,0 +1,5 @@
+module Corneal
+  module Test
+    VERSION = "0.1.0"
+  end
+end
